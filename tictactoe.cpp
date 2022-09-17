caf1666a79747946c7004a52f9e830baaa990f13
Shivesh Kumar Rai <105675048+shiv071@users.noreply.github.com>
2022-09-17 12:17:47 +0530

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,76 +1,7 @@
 #include <iostream>
 using namespace std;
 
-char square[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
-int checkwin();
-void board();
-
-int main()
-{
-    int player = 1, i, choice;
-
-    char mark;
-    do
-    {
-        board();
-        player = (player % 2) ? 1 : 2;
-
-        cout << "Player " << player << ", enter a number:  ";
-        cin >> choice;
-
-        mark = (player == 1) ? 'X' : 'O';
-
-        if (choice == 1 && square[1] == '1')
-
-            square[1] = mark;
-        else if (choice == 2 && square[2] == '2')
-
-            square[2] = mark;
-        else if (choice == 3 && square[3] == '3')
-
-            square[3] = mark;
-        else if (choice == 4 && square[4] == '4')
-
-            square[4] = mark;
-        else if (choice == 5 && square[5] == '5')
-
-            square[5] = mark;
-        else if (choice == 6 && square[6] == '6')
-
-            square[6] = mark;
-        else if (choice == 7 && square[7] == '7')
-
-            square[7] = mark;
-        else if (choice == 8 && square[8] == '8')
-
-            square[8] = mark;
-        else if (choice == 9 && square[9] == '9')
-
-            square[9] = mark;
-        else
-        {
-            cout << "Invalid move ";
-
-            player--;
-            cin.ignore();
-            cin.get();
-        }
-        i = checkwin();
-
-        player++;
-    } while (i == -1);
-    board();
-    if (i == 1)
-
-        cout << "==>\aPlayer " << --player << " win ";
-    else
-        cout << "==>\aGame draw";
-
-    cin.ignore();
-    cin.get();
-    return 0;
-}
+char square[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 /*********************************************
     FUNCTION TO RETURN GAME STATUS
@@ -121,7 +52,7 @@ void board()
     system("cls");
     cout << "\n\n\tTic Tac Toe\n\n";
 
-    cout << "Player 1 (X)  -  Player 2 (O)" << endl
+    cout << "Player 1 (X)  &  Player 2 (O)" << endl
          << endl;
     cout << endl;
 
@@ -140,4 +71,78 @@ void board()
 
     cout << "     |     |     " << endl
          << endl;
+}
+
+int main()
+{
+    int player = 1, i, choice;
+
+    char mark;
+    do
+    {
+        board();
+        // player = (player % 2) ? 1 : 2;
+        if (player%2!=0)
+        {
+            player=1;
+        }
+        else
+        {
+            player=2;
+        }
+
+        cout << "Player " << player << ", enter a number:  ";
+        cin >> choice;
+
+        mark = (player == 1) ? 'X' : 'O';
+
+        if (choice == 1 && square[1] == '1')
+
+            square[1] = mark;
+        else if (choice == 2 && square[2] == '2')
+
+            square[2] = mark;
+        else if (choice == 3 && square[3] == '3')
+
+            square[3] = mark;
+        else if (choice == 4 && square[4] == '4')
+
+            square[4] = mark;
+        else if (choice == 5 && square[5] == '5')
+
+            square[5] = mark;
+        else if (choice == 6 && square[6] == '6')
+
+            square[6] = mark;
+        else if (choice == 7 && square[7] == '7')
+
+            square[7] = mark;
+        else if (choice == 8 && square[8] == '8')
+
+            square[8] = mark;
+        else if (choice == 9 && square[9] == '9')
+
+            square[9] = mark;
+        else
+        {
+            cout << "Invalid move ";
+
+            player--;
+            cin.ignore();
+            cin.get();
+        }
+        i = checkwin();
+
+        player++;
+    } while (i == -1);
+    board();
+    if (i == 1)
+
+        cout << "==>Player " << --player << " win "<<endl;
+    else
+        cout << "==>Game draw"<<endl;
+    
+    // cin.ignore();
+    // cin.get();
+    return 0;
 }
